1a05dc6a7d7aaada37eb58a0a146474917ac443c
André Wantil <wantiltrabalhos@gmail.com>
2024-08-07 08:47:42 -0300

    erros corrigidos e codigo atualizado

--- codigos/script.js
+++ codigos/script.js
@@ -9,29 +9,34 @@ formulario.onsubmit = (event) => {
   // Sua lógica aqui!
 }
 
+// Função que calcula o IMC
 function calcularIMC() {
   
-  var imc = massa.value / (altura.value * altura.value);
+  let imc = parseFloat(massa.value / (altura.value * altura.value));
+
+  let resultado = document.getElementById('result')
 
   if (massa.value <= 0) {
-    document.getElementById('result').textContent = 'O número inserido no peso é inválido, insira outro'
+    resultado.textContent = 'O número inserido no peso é inválido, insira outro'
   } else if (altura.value <= 0) {
-    document.getElementById('result').textContent = 'O número inserido na altura é inválido, insira outro'
+    resultado.textContent = 'O número inserido na altura é inválido, insira outro'
   } else if (imc < 17) {
-    document.getElementById('result').textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está muito abaixo do peso ideal para sua altura!`
+    resultado.textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está muito abaixo do peso ideal para sua altura!`
   } else if (imc >= 17 && imc <= 18.49) {
-    document.getElementById('result').textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está abaixo do peso ideal para a sua altura!`
+    resultado.textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está abaixo do peso ideal para a sua altura!`
   } else if (imc >= 18.5 && imc <= 24.99) {
-    document.getElementById('result').textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está no peso ideal para a sua altura!`
+    resultado.textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está no peso ideal para a sua altura!`
   } else if (imc >= 25 && imc <= 29.99) {
-    document.getElementById('result').textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está acima do peso para a sua altura!`
+    resultado.textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está acima do peso para a sua altura!`
   } else if (imc >= 30 && imc <= 34.99) {
-    document.getElementById('result').textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está com obesidade - grau I!`
+    resultado.textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está com obesidade - grau I!`
   } else if (imc >= 35 && imc <= 39.99) {
-    document.getElementById('result').textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está com obesidade - II (severa)!`
+    resultado.textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está com obesidade - II (severa)!`
   } else if (imc >= 40) {
-    document.getElementById('result').textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está com obesidade - III (mórbida)!`
-  } else{}
+    resultado.textContent = `Seu IMC é de ${imc.toFixed(2)}, Você está com obesidade - III (mórbida)!`
+  } else {
+    console.error('Algo deu errado no codigo maluco')
+  }
   
   console.log(imc);
 }
